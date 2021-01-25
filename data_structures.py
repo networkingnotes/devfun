d24063759445cc821569c283bad069c1f59134bf
Bill <bill@burnam.net>
2021-01-25 12:35:09 -0600

    data structures examples added

---
+++ data_structures.py
@@ -0,0 +1,16 @@
+def legalAges():
+    lifeEvent = (
+        16, #driving,
+        18, #voting
+        21, #drinking
+        65  #retirement
+    )
+    print("The legal driving age is "
+      + str(lifeEvent[0]) + ".")
+    print("The legal voting age is "
+      + str(lifeEvent[1]) + ".")
+    print("The legal drinking age is "
+      + str(lifeEvent[2]) + ".")
+    print("The legal retirement age is "
+      + str(lifeEvent[3]) + ".")
+legalAges()
